b59c1539166f8452f1cbb2b8f1f97142b4767976
Mehedi Hassan <rehan51322@gmail.com>
2023-08-21 18:30:25 +0600

    banner added

--- src/components/UI/Home/Banner.js
+++ src/components/UI/Home/Banner.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Banner = () => {
   return (
-    <div>
-      <h2 className='text-4xl'>This is Banner</h2>
+    <div className="w-full h-screen">
+      <main className="grid grid-cols-1 md:grid-cols-2 gap-3 ">
+        <div className="w-full h-full flex flex-col justify-center pl-16">
+          <h2 className="text-4xl font-bold font-mono text-gray-700 ">
+            Want To Build Your <br /> DREAM PC?
+          </h2>
+          <p className="text-gray-500">
+            Ready to bring your dream PC to life? Imagine a powerhouse machine,
+            meticulously tailored to your needs and desires. From the
+            lightning-fast processors that make multitasking a breeze, to the
+            graphics cards that transport you into immersive worlds, every
+            component is a piece of the puzzle. At PC Builder, we are not just
+            selling PC parts, we are offering you the chance to create a
+            masterpiece, a digital canvas where speed, performance, and style
+            unite!
+          </p>
+          <button className="my-4 text-left">
+            <Link
+              className="px-5 py-1 bg-[#00bfff] text-white rounded"
+              href="/pc-builder"
+            >
+              PC Builder
+            </Link>
+          </button>
+        </div>
+        <div>
+          <Image
+            src="/pc.jpg"
+            width={100}
+            height={100}
+            layout="responsive"
+            alt="banner/img"
+          ></Image>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,12 +1,50 @@
+import Head from "next/head";
+// import styles from "@/styles/Login.module.css";
+import { useForm } from "react-hook-form";
 import RootLayout from "@/components/Layouts/RootLayout";
-import React from "react";
+import Link from "next/link";
 
-const pcBuilder = () => {
-  return <div>build your own pc</div>;
+const SignUp = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    // You can handle form submission here
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Next Sign Up</title>
+      </Head>
+      <div className="form">
+        <h3>SIGN UP</h3>
+        <hr />
+        <form className="text-left" onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="">Your Email</label>
+          <input {...register("email", { required: true })} type="email" />
+          <label htmlFor="">Your Password</label>
+          <input
+            {...register("password", { required: true })}
+            type="password"
+          />
+          <button className="bg-purple-500 !w-full rounded" type="submit">
+            Sign Up
+          </button>
+          <p className="py-3">
+            Already have an account?{" "}
+            <Link className="text-blue-500" href="/login">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default pcBuilder;
+export default SignUp;
 
-pcBuilder.getLayout = function getLayout({ page }) {
+SignUp.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
